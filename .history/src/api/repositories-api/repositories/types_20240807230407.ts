b8953f5216b5136ddc794a26de2a87c781e60682
Мнац Аветисян <mnatssteam@mail.ru>
2024-08-07 23:06:05 +0300

    feat(GRS-1): create interceptor

---
+++ .history/src/api/repositories-api/repositories/types_20240807230407.ts
@@ -0,0 +1,17 @@
+export type Repository = {
+  id: number;
+  name: string;
+  language: string;
+  forks_count: number;
+  stargazers_count: number;
+  updated_at: string;
+  description: string;
+  license: {
+    name: string;
+  };
+};
+
+export type RepositoriesState = {
+  repositories: Repository[];
+  status: "idle" | "loading" | "failed";
+};
