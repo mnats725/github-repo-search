867cceb373626d0e18c4085fe614b80fe40b37bc
Мнац Аветисян <mnatssteam@mail.ru>
2024-08-08 03:39:18 +0300

    feat(GRS-2): create TableDetails component

--- src/components/lib/table/table-details/table-details.tsx
+++ src/components/lib/table/table-details/table-details.tsx
@@ -11,33 +11,31 @@ type RepositoryDetailsProps = {
   selectedRepo: Repository | null;
 };
 
-export const TableDetails: FC<RepositoryDetailsProps> = ({ drawerOpen, setDrawerOpen, selectedRepo }) => {
-  return (
-    <Drawer
-      anchor="right"
-      open={drawerOpen}
-      onClose={() => setDrawerOpen(false)}
-      sx={{ width: 400, flexShrink: 0, "& .MuiDrawer-paper": { width: 400 } }}
-    >
-      <Box sx={{ padding: 2 }}>
-        <ConditionalRender conditions={[!!selectedRepo]}>
-          <>
-            <Typography variant="h6">{selectedRepo?.name}</Typography>
-            <Typography>Description: {selectedRepo?.description}</Typography>
-            <Typography>License: {selectedRepo?.license.name}</Typography>
-            <Typography>Language: {selectedRepo?.language}</Typography>
-            <Typography>Forks: {selectedRepo?.forks_count}</Typography>
-            <Typography>Stars: {selectedRepo?.stargazers_count}</Typography>
-            <Typography>
-              Last Updated:
-              <ConditionalRender conditions={[!!selectedRepo?.updated_at]}>
-                {new Date(selectedRepo?.updated_at || "").toLocaleDateString()}
-              </ConditionalRender>
-              <ConditionalRender conditions={[!selectedRepo?.updated_at]}>N/A</ConditionalRender>
-            </Typography>
-          </>
-        </ConditionalRender>
-      </Box>
-    </Drawer>
-  );
-};
+export const TableDetails: FC<RepositoryDetailsProps> = ({ drawerOpen, setDrawerOpen, selectedRepo }) => (
+  <Drawer
+    anchor="right"
+    open={drawerOpen}
+    onClose={() => setDrawerOpen(false)}
+    sx={{ width: 400, flexShrink: 0, "& .MuiDrawer-paper": { width: 400 } }}
+  >
+    <Box sx={{ padding: 2 }}>
+      <ConditionalRender conditions={[!!selectedRepo]}>
+        <>
+          <Typography variant="h6">{selectedRepo?.name}</Typography>
+          <Typography>Description: {selectedRepo?.description}</Typography>
+          <Typography>License: {selectedRepo?.license.name}</Typography>
+          <Typography>Language: {selectedRepo?.language}</Typography>
+          <Typography>Forks: {selectedRepo?.forks_count}</Typography>
+          <Typography>Stars: {selectedRepo?.stargazers_count}</Typography>
+          <Typography>
+            Last Updated:
+            <ConditionalRender conditions={[!!selectedRepo?.updated_at]}>
+              {new Date(selectedRepo?.updated_at || "").toLocaleDateString()}
+            </ConditionalRender>
+            <ConditionalRender conditions={[!selectedRepo?.updated_at]}>N/A</ConditionalRender>
+          </Typography>
+        </>
+      </ConditionalRender>
+    </Box>
+  </Drawer>
+);
