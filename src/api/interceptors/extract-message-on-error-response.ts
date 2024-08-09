3e4f2d74a03a5fccaa1117774fdf1812b4c01bf2
Мнац Аветисян <mnatssteam@mail.ru>
2024-08-09 05:39:39 +0300

    refactor(GRS-3): ui restruct

--- src/api/interceptors/extract-message-on-error-response.ts
+++ src/api/interceptors/extract-message-on-error-response.ts
@@ -2,21 +2,27 @@ import type { AxiosError } from "axios";
 
 import type { ServiceError } from "../../types/service-error.type";
 
+// Функция для извлечения сообщения из ошибки ответа
 export const extractMessageOnErrorResponse = async (
   error: AxiosError<ServiceError>
 ): Promise<AxiosError<ServiceError>> => {
   const { response } = error;
 
+  // Если в ошибке есть ответ
   if (response) {
+    // Извлекаем сообщение, удаляя всё до последнего двоеточия
     const extractedMessage = response.data.message.split(":").at(-1)?.trim() || "";
 
+    // Создаем новый объект ошибки с извлеченным сообщением
     const errorWithExtractedMessage = {
       ...response.data,
       message: extractedMessage,
     };
 
+    // Возвращаем ошибку с новым сообщением
     return Promise.reject(errorWithExtractedMessage);
   }
 
+  // Если ответа нет, возвращаем исходную ошибку
   return Promise.reject(error);
 };
